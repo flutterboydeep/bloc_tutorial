7a0b80b94a0a75eba3e714c3c7c73059fca631ed
Your Name <you@example.com>
2024-02-01 16:27:49 +0530

    counter inc-dec using bloc in privious branch qubit but in this branch using bloc

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -1,25 +1,21 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:learn_bloc/bloc/counter_bloc.dart';
 import 'package:learn_bloc/cubit/AdderPart.dart';
-import 'package:learn_bloc/cubit/CounterCubit.dart';
 
-class MyHomePage extends StatefulWidget {
+class MyHomePage extends StatelessWidget {
   const MyHomePage({super.key, required this.title});
 
   final String title;
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
   Widget build(BuildContext context) {
-    final counterCubit = BlocProvider.of<CounterCubit>(context);
+    // final counterCubit = BlocProvider.of<CounterCubit>(context);
+    // final counterBloc = BlocProvider.of<CounterBloc>(context);
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
+        title: Text(title),
       ),
       body: Center(
         child: Column(
@@ -28,14 +24,14 @@ class _MyHomePageState extends State<MyHomePage> {
             const Text(
               'You have pushed the button this many times:',
             ),
-            BlocBuilder(
-                bloc: counterCubit,
-                builder: (context, UpdatedValue) {
-                  return Text(
-                    '$UpdatedValue',
-                    style: Theme.of(context).textTheme.headlineMedium,
-                  );
-                }),
+            BlocBuilder<CounterBloc, int>(
+                // bloc: counterBloc,
+                builder: (context, updatedValue) {
+              return Text(
+                '$updatedValue',
+                style: Theme.of(context).textTheme.headlineMedium,
+              );
+            }),
           ],
         ),
       ),
